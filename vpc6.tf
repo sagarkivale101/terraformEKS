580a383a10753fbc13dfbb3f6fed615308d03936
sagarkivale101 <56817030+sagarkivale101@users.noreply.github.com>
2022-06-27 11:06:50 +0530

    Update vpc6.tf

--- vpc6.tf
+++ vpc6.tf
@@ -10,7 +10,7 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "gessa-test-${random_string.suffix.result}"
+  cluster_name = "gessa-${random_string.suffix.result}"
 }
 
 resource "random_string" "suffix" {
