d8bc298cd020b9b3b2af64777983ea85006ca56b
sagarkivale101 <56817030+sagarkivale101@users.noreply.github.com>
2022-07-08 15:07:48 +0530

    Update eks-cluster1.tf

--- eks-cluster1.tf
+++ eks-cluster1.tf
@@ -13,11 +13,11 @@ module "eks" {
 
   worker_groups = [
     {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
+      name                          = "workergrpname"
+      instance_type                 = "instanceType"
       additional_userdata           = "echo foo bar"
       additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-      asg_desired_capacity          = 2
+      asg_desired_capacity          = capacity
     }
     # {
     #   name                          = "worker-group-2"
